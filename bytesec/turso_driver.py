8b71c722f327852719316594a164a6b2fb09a89e
huang <frcrsppcc@gmail.com>
2026-06-12 23:32:46 +0700

    fix: add no-op create_function to TursoConnection (SQLAlchemy SQLite dialect needs it)

--- bytesec/turso_driver.py
+++ bytesec/turso_driver.py
@@ -178,6 +178,9 @@ class TursoConnection:
     def commit(self):
         pass  # HTTP mode auto-commits
 
+    def create_function(self, name, num_params, func):
+        pass  # Turso HTTP driver doesn't support custom SQL functions
+
     def rollback(self):
         pass
 
